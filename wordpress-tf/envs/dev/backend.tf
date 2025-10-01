a6fb47621272f5dbe099de07b206a55af0389d88
Yassin Suleiman <yassine.suleiman@proton.me>
2025-10-01 02:27:24 +0200

    cleanup + optmization of code

--- wordpress-tf/envs/dev/backend.tf
+++ wordpress-tf/envs/dev/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "wordpressbucket-yassin"
     key            = "dev/terraform.tfstate"
     region         = "eu-north-1"
-    dynamodb_table = "terraform-locks" # optional but recommended
     encrypt        = true
   }   
 }
